8000e7fdfe6b71497dd851cfd43c1a50b55a50d9
Lucas Ferreira da Silva <lucferreira14@gmail.com>
2021-08-02 23:03:04 -0300

    fist commit

---
+++ src/main/resources/static/js/script.js
@@ -0,0 +1,9 @@
+
+function showCardAddEvent(){
+    var card = document.querySelector(".formCard");
+    card.style.display = "block";
+}
+function hideCardAddEvent(){
+    var card = document.querySelector(".formCard");
+    card.style.display = "none";
+}
